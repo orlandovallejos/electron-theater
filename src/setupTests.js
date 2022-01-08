7243479f063d53d6fd81531505c23ddf2b797e17
Orlando Vallejos <orlando@zlapps.com>
2022-01-07 19:38:12 -0500

    Add the "jest-react-hooks-shallow" to test the "useEffect()" method

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,9 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import enableHooks from 'jest-react-hooks-shallow';
 
+// This is the enzyme adapter:
 configure({ adapter: new Adapter() });
+
+// This is to be able to run "useEffect()" in shallow rendering:
+enableHooks(jest);
